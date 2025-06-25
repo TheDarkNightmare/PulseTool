164c696281f2c1d529d28c9617338d2ec1d90d75
Mariusz "Nightmare" Jarnot <mariusz.jarnot@onet.pl>
2025-06-25 07:00:02 +0200

    Update base-package.sh

--- src/base-package.sh
+++ src/base-package.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Function to install yay if not installed
-install_yay() {
+install_aur_systeminit() {
     if ! command -v yay &> /dev/null; then
         echo -e "                       [\e[31m----------------- System Checking ----------------\e[0m]"
-        echo "Yay package is not installed. Installing yay for AUR ..."
+        echo "Paru package is not installed. Installing Paru backend for AUR ..."
         sudo pacman -S --needed --noconfirm base-devel
-        git clone https://aur.archlinux.org/yay.git
-        cd yay
+        git clone https://aur.archlinux.org/paru.git
+        cd paru
         makepkg -si --noconfirm
         cd ..
-        rm -rf yay
+        rm -rf paru
     fi
 }
