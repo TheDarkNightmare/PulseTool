ba81df3cb168c85833a8474f8a08f83ea0119308
Mariusz "Nightmare" Jarnot <mariusz.jarnot@onet.pl>
2025-08-05 09:40:13 +0200

    Detect Wayland at runtime and add wayland-friendly display query

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,10 +1,18 @@
 #include <QApplication>
+#include <QDebug>
 
 #include "MainWindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    if (qEnvironmentVariableIsSet("WAYLAND_DISPLAY")) {
+        qDebug() << "WAYLAND_DISPLAY detected. Using Wayland session.";
+    } else {
+        qDebug() << "WAYLAND_DISPLAY not set. Falling back to X11 session.";
+    }
+
     MainWindow w;
     w.show();
     return app.exec();
